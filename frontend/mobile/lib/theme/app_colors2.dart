33abb0bffbdce1d8fc29912bd95d4d82e4b37814
Gleb Galkin <60242492+glebgalkin@users.noreply.github.com>
2024-04-06 16:14:21 -0400

    Create app_colors2.dart

---
+++ frontend/mobile/lib/theme/app_colors2.dart
@@ -0,0 +1,11 @@
+// Define the color palette for Flutter
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color businessName = Color(0xFF336A86);
+  static const Color slogan = Color(0xFF8EAEC4);
+  static const Color icon = Color(0xFF283132);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color backgroundElements = Color(0xFF336A86);
+  static const Color foregroundElements = Color(0xFFFFFFFF);
+}
