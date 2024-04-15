d00feda28e40183a0a37077e158dede5fbfd5e5f
Gleb Galkin <gleb.galkin@bellmedia.ca>
2024-04-14 22:32:52 -0400

    Added upsert for duplicates

--- backend/recommendation-validator/src/service/input-processor.ts
+++ backend/recommendation-validator/src/service/input-processor.ts
@@ -5,13 +5,14 @@ import {validateRecommendation} from "./recommendation-validator";
 import {parseUserMeta} from "./user-meta-parser";
 import {UserMeta} from "../types/user-meta";
 import {mapRecommendation} from "./recommendation-mapper";
-import {saveRecommendation} from "../repository/recommendation-repository";
+import {upsertRecommendation} from "../repository/recommendation-repository";
 
 export const processInput = async (event: APIGatewayProxyEvent, client: MongoClient) => {
     console.log(event)
+    const db = await client.db(process.env.MONGODB_NAME);
 
     const userMeta: UserMeta = parseUserMeta(event)
     const feRecommendation: FERecommendation = validateRecommendation(event)
     const recommendation: Recommendation = mapRecommendation(userMeta, feRecommendation)
-    return await saveRecommendation(recommendation, client)
+    return await upsertRecommendation(recommendation, db)
 }
