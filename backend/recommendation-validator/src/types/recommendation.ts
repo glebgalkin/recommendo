26bfe021015ea4fa8be1d15b3e1a8c7b8840aeef
Gleb Galkin <gleb.galkin@bellmedia.ca>
2024-04-16 22:56:03 -0400

    [BE] Added googleApi data fields.

--- backend/recommendation-validator/src/types/recommendation.ts
+++ backend/recommendation-validator/src/types/recommendation.ts
@@ -1,7 +1,13 @@
 export type FERecommendation = {
     title: string,
     description?: string,
-    address?: string,
+    address?: {
+        apartmentUnit?: string,
+        street: string,
+        city: string,
+        province: string,
+        postalCode: string,
+    }
     socials: {
         instagram: string,
         [social: string]: string,
@@ -18,13 +24,31 @@ export type Recommendation = {
     email: string,
     title: string,
     description?: string,
-    address: string | null
+    address: Address | null,
     socials: {
         instagram: string,
         [social: string]: string,
     },
     city: {
         name: string,
-        id: string
-    }
+        id: string,
+    },
+    googleApi: GoogleApi | null;
+}
+
+export type GoogleApi = {
+    businessStatus: string,
+    rating?: number,
+    tags?: string[],
+    userRatingsTotal?: number
+    name?: string
+}
+
+export type Address = {
+    apartmentUnit?: string,
+    street: string,
+    city: string,
+    province: string,
+    postalCode: string,
+    providedBy: string
 }
--- backend/recommendation-validator/tests/google-map-parser.test.ts
+++ backend/recommendation-validator/tests/google-map-parser.test.ts
@@ -1,8 +1,5 @@
 import {describe, it, vi, expect} from "vitest";
 import {generateGoogleApiUrl} from "../src/constants/maps";
-import {getAddressByGoogle, parseGoogleApiAddress} from "../src/service/address-parser";
-import axios, {AxiosResponse} from "axios";
-import {FERecommendation} from "../src/types/recommendation";
 
 describe('Google map api functionality unit tests', () => {
 
@@ -25,44 +22,5 @@ describe('Google map api functionality unit tests', () => {
             expect(resultUrl).toBe(expectedUrl);
         });
     })
-
-    it('test the address parser from google response', () => {
-        const mockApiResponse = {
-            data: {
-                results: [{
-                    formatted_address: '123 Coffee Lane, Beanville, CA'
-                }]
-            }
-        };
-
-        let result = parseGoogleApiAddress(mockApiResponse as unknown as AxiosResponse);
-        expect(result).toBe('123 Coffee Lane, Beanville, CA');
-
-        const emptyApiResponse = {
-            data: {
-                results: []
-            }
-        };
-
-        result = parseGoogleApiAddress(emptyApiResponse as unknown as AxiosResponse);
-        expect(result).toBeNull();
-    })
-
-    it('should call axios with the correct URL and return the address', async () => {
-        vi.mock('axios');
-        const mockApiResponse = {
-            data: {
-                results: [{
-                    formatted_address: '123 Coffee Lane, Beanville, CA'
-                }]
-            }
-        };
-        vi.mocked(axios.get).mockResolvedValue(mockApiResponse);
-
-        const feRecommendation = { title: 'Coffee Shop', city: { name: 'Beanville' } };
-        const address = await getAddressByGoogle(feRecommendation as unknown as FERecommendation);
-
-        expect(address).toBe('123 Coffee Lane, Beanville, CA');
-    });
 
 })
