40442a41cf8b73d364edadfb9e367ca3b9817d66
Konyaka1 <alexey.chernyakov00@gmail.com>
2024-04-19 14:57:41 -0400

    [FE] Added localization

--- frontend/mobile/lib/app/auth/view/profile_page.dart
+++ frontend/mobile/lib/app/auth/view/profile_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
+import 'package:recommendo/l10n/l10n.dart';
 import 'package:recommendo/navigation/app_paths.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -11,7 +11,7 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ProfileScreen(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: Text(context.l10n.profilePageAppBarTitle),
       ),
       actions: [
         SignedOutAction((context) => context.go(AppPaths.signInPage)),
