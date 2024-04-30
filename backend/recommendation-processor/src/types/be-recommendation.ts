23c5f13f6061f21fcbaa8b0b2c1955745f38625a
Gleb Galkin <gleb.galkin@bellmedia.ca>
2024-04-29 23:16:09 -0400

    [BE] Removed comments

--- backend/recommendation-processor/src/types/be-recommendation.ts
+++ backend/recommendation-processor/src/types/be-recommendation.ts
@@ -1,6 +1,3 @@
-import {Recommendation, Source} from "./recommendation";
-import {SourceType} from "../constants/source-types";
-
 export type FeedRecommendation = {
     cityId: string,
     instagram?: string,
@@ -13,27 +10,3 @@ export type FeedRecommendation = {
     recommendoCounter: number,
     tags: string[]
 }
-
-
-/*
-                FeedRecommendation
-
-                {
-                    "cityId": "12345",
-                    "instagram": "exampleBistro",
-                    "googleMapId": "8PWX7QF2+77",
-                    "website": "https://www.examplebistro.com",
-                    "businessName": "Example Bistro",
-                    "description": "A cozy and inviting place offering a fusion of local and international cuisines.",
-                    "image": "https://www.examplebistro.com/image.jpg",
-                    "lastCrawledDate": "2024-04-28T00:00:00Z",
-                    "recommendoCounter": 150,
-                    "tags": ["gourmet", "local", "fusion"]
-                }
- */
-
-
-
-
-
-
